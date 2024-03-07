596bda236534cbe95f1d281d8d22eeaecef32228
Jennifer Oreffo <jennyoreffo@gmail.com>
2024-03-07 06:53:11 -0800

    commit

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+	constructor(name, length, students) {
+		this.name = name;
+		this.length = length;
+		this.students = students;
+	}
+// Getter - name //
+	get name() {
+		return this._name;
+	}
+// setter- name.//
+	set name(value) {
+		if (typeof value !== 'string') {
+			throw new TypeError('Name must be a string');
+		}
+		this._name = value;
+	}
+// getter - length //
+	get length() {
+		return this._length;
+	}
+// setter - length //
+	set length(value) {
+		if (typeof value !== 'number') {
+			throw new TypeError('Length must be a number');
+		}
+		this._length = value;
+	}
+// getter - student //
+	get students() {
+		return this._students;
+	}
+// set student //
+	set students(value) {
+		if (!(value instanceof Array)) {
+			throw new TypeError('Students must be an array of strings');
+		}
+		if (!value.every((student) => typeof student === 'string')) {
+			throw new TypeError('Students must be an array of strings');
+		}
+		this._students = value;
+	}
+}
+
+
