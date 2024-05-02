62b7535d5685335b6218874b20989b7b581c80eb
Jennifer Oreffo <jennyoreffo@gmail.com>
2024-05-02 08:51:05 -0700

    commit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,13 @@
-// 1-stdin.js
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const readline = require('readline');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-console.log("Welcome to Holberton School, what is your name?");
-
-rl.on('line', (input) => {
-  if (input === 'exit') {
-    console.log("This important software is now closing");
-    rl.close();
-  } else {
-    console.log(`Your name is: ${input}`);
+  if (name.length > 0) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
 
+process.stdin.on('exit', () => {
+  process.stdout.write('This important software is now closing\n');
+});
